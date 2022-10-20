e5245c6ee91c47709bbf177295e20e0839fc4efa
eatilola <eatilola@iu.edu>
2022-10-20 17:00:50 -0400

    C4

--- Projects/js/app.js
+++ Projects/js/app.js
@@ -1,319 +1,184 @@
+let p1= document.getElementById("player1");
+let p2 = document.getElementById("player2");
+let resetButton = document.getElementById("resetButton");
 
+var playerRed = "R";
+var playerYellow = "Y";
+let p1Score = 0;
+let p2Score = 0;
 
-var grid = [
-    [1, 2, 3, 4, 5, 6, 7],
-  
-    [8, 9, 10, 11, 12, 13, 14],
-  
-    [15, 16, 17, 18, 19, 20, 21],
+var currPlayer = playerRed;
+var currColumns;
 
-    [22, 23, 24, 25, 26, 27, 28],
-  
-    [29, 30, 31, 32, 33, 34, 35],
-  
-    [36, 37, 38, 39, 40, 41, 42],
-  ];
-  
-  var markers = [
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " "
-  ];
-  
-  
-  var userWin = 0;
-  var computerWin = 0;
-  var tieWin = 0;
-  
-  console.log(grid);
-  
-  let spot = document.getElementsByClassName("spot");
-  let background = document.getElementById("background");
-  let title = document.getElementById("title");
-  let winner = document.getElementById("winner");
-  let resetButton = document.getElementById("resetButton");
-  let user = document.getElementById("user");
-  let computer = document.getElementById("computer");
-  let tie = document.getElementById("tie");
-  let goameovercover = document.getElementById("gameovercover");
-  
-  // resetButton.style.display = "none";
-  
-  for (i = 0; i < spot.length; i++) {
-    spot[i].addEventListener("click", spotClicked);
-  }
+var columns = 7;
+var rows = 6;
 
+var gameOver = false;
 
-  
-  count = 0;
-  function spotClicked(event) {
-    let answer = event.target.getAttribute("data-spot");
-  
-    if (event.target.style.color == "##FF0000" || event.target.innerHTML == "0") {
-      console.log("nope stop taken");
-    } else if (count % 2 == 0) {
-      event.target.innerHTML = "O";
-      event.target.style.color = "#FFFF00";
-      event.target.style.fontSize = "100px";
-      event.target.style.justifyContent = "center";
-      count = count + 1;
-    } else {
-      event.target.innerHTML = "O";
-      event.target.style.color = "#FF0000";
-      count = count + 1;
-      // event.target.innerHTML = "O";
-      event.target.style.fontSize = "100px";
-      event.target.style.justifyContent = "center";
-    }
+var gameCount = 0;
 
-    markCount = 0; 
-    for(var i = 0; i < grid.length; i++) {
-        for(var j = 0; j < grid[i].length; j++) {
-            console.log(grid[i][j]);
-            console.log(event.target.style.color) // red
-            if (event.target.style.color == "#FFFF00" && answer == grid[i][j]) {
-                markers[markCount++] = "O";
-            }
-            else if (event.target.target.style.color == "#FF0000" && answer == grid[i][j]) {
-                markers[markCount++] = "X";
-            }
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    board = [];
+
+    currColumns = [5,5,5,5,5,5,5]
+
+    for(let r =0; r< rows; r++){
+        let row =[];
+        for(let c= 0; c<columns; c++){
+            row.push(' ');
+
+            // <div id= "0-0" class = "tile"> </div>
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.addEventListener("click", setPiece);
+            document.getElementById("board").append(tile);
+        }
+        board.push(row);
     }
 }
-    // if (event.target.innerHTML.style.color == "#633191" && answer == grid[0][0]) {
-    //   marker1 = "o";
-    // } else if (event.target.innerHTML.style.color == "#633191" && answer == grid[0][1]) {
-    //   marker2 = "o";
-    // } else if (event.target.innerHTML.style.color == "#633191" && answer == grid[0][2]) {
-    //   marker3 = "o";
-    // } else if (event.target.innerHTML.style.color == "#633191" && answer == grid[0][3]) {
-    //     marker4 = "o";
-    // } else if (event.target.innerHTML.style.color == "#633191" && answer == grid[0][4]) {
-    //     marker5 = "o";
-    // } else if (event.target.innerHTML.style.color == "#633191" && answer == grid[0][5]) {
-    //     marker6 = "o";
-    // } else if (event.target.innerHTML.style.color == "#633191" && answer == grid[0][6]) {
-    //     marker7 = "o";
-    // } else if (event.target.innerHTML.style.color == "#633191" && answer == grid[1][0]) {
-    //   marker8 = "o";
-    // } else if (event.target.innerHTML.style.color == "#633191" && answer == grid[1][1]) {
-    //   marker9 = "o";
-    // } else if (event.target.innerHTML.style.color == "#633191" && answer == grid[1][2]) {
-    //   marker10 = "o";
-    // } else if (event.target.innerHTML.style.color == "#633191" && answer == grid[1][3]) {
-    //     marker11 = "o";
-    // } else if (event.target.innerHTML.style.color == "#633191" && answer == grid[1][4]) {
-    //     marker12 = "o";
-    // } else if (event.target.innerHTML.style.color == "#633191" && answer == grid[1][5]) {
-    //     marker13 = "o";
-    // } else if (event.target.innerHTML.style.color == "#633191" && answer == grid[1][6]) {
-    //     marker14 = "o";
-    // } else if (event.target.innerHTML.style.color == "#633191" && answer == grid[2][0]) {
-    //   marker15 = "o";
-    // } else if (event.target.innerHTML.style.color == "#633191" && answer == grid[2][1]) {
-    //   marker17 = "o";
-    // } else if (event.target.innerHTML.style.color == "#633191" && answer == grid[2][2]) {
-    //   marker18 = "o";
-    // } else if (event.target.innerHTML.style.color == "#633191" && answer == grid[2][3]) {
-    //     marker7 = "o";
-    // } else if (event.target.innerHTML.style.color == "#633191" && answer == grid[2][4]) {
-    //     marker8 = "o";
-    // } else if (event.target.innerHTML.style.color == "#633191" && answer == grid[2][5]) {
-    //     marker9 = "o";
-    // } 
-      
-    
-    
-    /*if (event.target.innerHTML.style.color == "#FF0000" && answer == grid[0][0]) {
-      marker1 = "x";
-    } else if (event.target.innerHTML.style.color == "#FF0000" && answer == grid[0][1]) {
-      marker2 = "x";
-    } else if (event.target.innerHTML.style.color == "#FF0000" && answer == grid[0][2]) {
-      marker3 = "x";
-    } else if (event.target.innerHTML.style.color == "#FF0000" && answer == grid[1][0]) {
-      marker4 = "x";
-    } else if (event.target.innerHTML.style.color == "#FF0000" && answer == grid[1][1]) {
-      marker5 = "x";
-    } else if (event.target.innerHTML.style.color == "#FF0000" && answer == grid[1][2]) {
-      marker6 = "x";
-    } else if (event.target.innerHTML.style.color == "#FF0000" && answer == grid[2][0]) {
-      marker7 = "x";
-    } else if (event.target.innerHTML.style.color == "#FF0000" && answer == grid[2][1]) {
-      marker8 = "x";
-    } else if (event.target.innerHTML.style.color == "#FF0000" && answer == grid[2][2]) {
-      marker9 = "x";
-    }*/
-  
-    // //left to right YELLOW
-    // if (marker1 == "x" && marker2 == "x" && marker3 == "x") {
-    //   player1();
-    // } else if (marker4 == "x" && marker5 == "x" && marker6 == "x") {
-    //   player1();
-    // } else if (marker7 == "x" && marker8 == "x" && marker9 == "x") {
-    //   player1();
-    // }
-  
-    // //left to right BLUE
-    // else if (marker1 == "o" && marker2 == "o" && marker3 == "o") {
-    //   player2();
-    // } else if (marker4 == "o" && marker5 == "o" && marker6 == "o") {
-    //   player2();
-    // } else if (marker7 == "o" && marker8 == "o" && marker9 == "o") {
-    //   player2();
-    // }
-  
-    // //up and down YELLOW
-    // else if (marker1 == "x" && marker4 == "x" && marker7 == "x") {
-    //   player1();
-    // } else if (marker2 == "x" && marker5 == "x" && marker8 == "x") {
-    //   player1();
-    // } else if (marker3 == "x" && marker6 == "x" && marker9 == "x") {
-    //   player1();
-    // }
-  
-    // //up and down BLUE
-    // else if (marker1 == "o" && marker4 == "o" && marker7 == "o") {
-    //   player2();
-    // } else if (marker2 == "o" && marker5 == "o" && marker8 == "o") {
-    //   player2();
-    // } else if (marker3 == "o" && marker6 == "o" && marker9 == "o") {
-    //   player2();
-    // }
-    // //diagonal YELLOW
-    // else if (marker1 == "x" && marker5 == "x" && marker9 == "x") {
-    //   player1();
-    // } else if (marker3 == "x" && marker5 == "x" && marker7 == "x") {
-    //   player1();
-    // }
-    // //diagonal BLUE
-    // else if (marker1 == "o" && marker5 == "o" && marker9 == "o") {
-    //   player2();
-    // } else if (marker3 == "o" && marker5 == "o" && marker7 == "o") {
-    //   player2();
-    // }
-  
-    //if all spots are filled
-    // else if (count == 9) {
-    //   resetButton.style.display = "block";
-    //   tieWin = tieWin + 1;
-    //   console.log("tie");
-    // }
-    // user.innerHTML = "Red: " + userWin;
-    // computer.innerHTML = "Yellow: " + computerWin;
-    // tie.innerHTML = "Tie: " + tieWin;
+
+function setPiece(){
+    if(gameOver){
+        return;
     }
 
-//   function resetGame() {
-//     console.log("reset game");
-//     count = 0;
-  
-//     let markers = [
-//         marker1 = " ",
-//       marker2 = " ",
-//       marker3 = " ",
-//       marker4 = " ",
-//       marker5 = " ",
-//       marker6 = " ",
-//       marker7 = " ",
-//       marker8 = " ",
-//       marker9 = " ",
-//       marker10 = " ",
-//       marker11 = " ",
-//       marker12 = " ",
-//       marker13 = " ",
-//       marker14 = " ",
-//       marker15 = " ",
-//       marker16 = " ",
-//       marker17 = " ",
-//       marker18 = " ",
-//       marker19 = " ",
-//       marker20 = " ",
-//       marker21 = " ",
-//       marker22 = " ",
-//       marker23 = " ",
-//       marker24 = " ",
-//       marker25 = " ",
-//       marker26 = " ",
-//       marker27 = " ",
-//       marker28 = " ",
-//       marker29 = " ",
-//       marker30 = " ",
-//       marker31 = " ",
-//       marker32 = " ",
-//       marker33 = " ",
-//       marker34 = " ",
-//       marker35 = " ",
-//       marker36 = " ",
-//       marker37 = " ",
-//       marker38 = " ",
-//       marker39 = " ",
-//       marker40 = " ",
-//       marker41 = " ",
-//       marker42 = " "
-//       ];
-  
-//     for (i = 0; i < spot.length; i++) {
-//       spot[i].addEventListener("click", spotClicked);
-//       spot[i].style.background = "#fff";
-//       spot[i].innerHTML = " ";
-//     }
-//     winner.innerHTML = " ";
-//     resetButton.style.display = "none";
-//     goameovercover.style.display = "none";
-//   }
-  
-//   function player1(event) {
-//     resetButton.style.display = "block";
-//     userWin = userWin + 1;
-//     goameovercover.style.display = "flex";
-//     console.log("PLayer 1 win");
-//   }
-  
-//   function player2() {
-//     resetButton.style.display = "block";
-//     computerWin = computerWin + 1;
-//     console.log("Player 2 win");
-//     goameovercover.style.display = "flex";
-//     for (i = 0; i >= 9; i++) {
-//       markers[i] = "y";
-//     }
-//   }
-  
+    let coords = this.id.split("-"); // "0-0" = ["0", "0"]
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+
+    r = currColumns[c];
+    if(r < 0){
+        return;
+    }
+
+    board[r][c] = currPlayer;
+    let tile = document.getElementById(r.toString() + "-" + c.toString());
+    if(currPlayer == playerRed){
+        tile.classList.add("red-piece");
+        gameCount++;
+        currPlayer = playerYellow;
+    }
+    else{
+        tile.classList.add("yellow-piece");
+        gameCount++;
+        currPlayer = playerRed;
+        
+    }
+    r -=1; //updating the row height for the column
+    currColumns[c] = r; //update the array
+    
+    checkTie();
+    checkWinner();
+    
+
+}
+
+function checkWinner(){
+    //horizontal check
+    for(let r = 0; r< rows; r++){
+        for(let c = 0; c < columns - 3; c++){
+            if(board[r][c] != ' '){
+               if((board[r][c] == board[r][c+1]) && (board[r][c+1] == board[r][c+2]) && (board[r][c+2] == board[r][c+3])){
+                    setWinner(r,c);
+                    return;
+                } 
+            }
+            
+        }
+    }
+
+    //Vertical Check
+    for(let c = 0; c < columns; c++){
+        for(let r = 0; r < rows - 3; r++){
+            if(board[r][c] != ' '){
+                if((board[r][c] == board[r+1][c]) && (board[r+1][c] == board[r+2][c]) && (board[r+2][c] == board[r+3][c])){
+                    setWinner(r,c);
+                    return;
+                } 
+             }
+        }
+    }
+
+    //Anti-Diagonal check
+    for(let r = 0; r < rows -3; r++){
+        for(let c = 0; c < columns-3; c++){
+            if(board[r][c] != ' '){
+                if((board[r][c] == board[r+1][c+1]) && (board[r+1][c+1] == board[r+2][c+2]) && (board[r+2][c+2] == board[r+3][c+3])){
+                    setWinner(r,c);
+                    return;
+                } 
+            }
+        }
+    }
+
+    //Diagonal check
+    for(let r = 0; r < rows; r++){
+        for(let c = 0; c < columns-3; c++){
+            if(board[r][c] != ' '){
+                if((board[r][c] == board[r-1][c+1]) && (board[r-1][c+1] == board[r-2][c+2]) && (board[r-2][c+2] == board[r-3][c+3])){
+                    setWinner(r,c);
+                    return;
+                } 
+            }
+        }
+    }
+}
+
+function setWinner(r,c){
+    let winner = document.getElementById("winner");
+
+
+
+
+    if(board[r][c] == playerRed){
+        p1Score = p1Score + 1;
+        winner.innerHTML = "Red Wins!";
+        TweenMax.from(winner, {duration:3, x:-2000});
+        TweenMax.from(winner, {duration:3, opacity: 0});
+        //p1.innerHTML = "Player 1: " + p1Score;
+        resetButton.style.display = "flex";
+        TweenMax.from(resetButton, {duration:3, opacity: 0});
+    }
+    else{
+        p2Score = p2Score +1 ;
+        winner.innerHTML = "Yellow Wins!"
+        TweenMax.from(winner, {duration:3, x:-2000});
+        //p2.innerHTML = "Player 2: " + p2Score;
+        resetButton.style.display = "flex";
+        TweenMax.from(resetButton, {duration:3, opacity: 0});
+    }
+
+    gameOver = true;
+}
+
+
+function checkTie(){
+    if(gameCount >=42){
+        gameOver = true;
+        winner.innerHTML = "Tie!"
+        TweenMax.from(winner, {duration:3, x:-2000});
+        resetButton.style.display = "flex";
+        TweenMax.from(resetButton, {duration:3, opacity: 0});
+    }
+
+}
+function resetGame(){
+    console.log("reset game");
+    location.reload();
+
+    // let winner = document.getElementById("winner");
+
+    // for(let r = 0; r < rows; r++){
+    //     for(let c = 0; c < columns; c++){
+    //         board[r][c] = ' ';
+    //     }
+    // }
+
+    // winner.innerHTML = "";
+    // resetButton.style.display = "none";
+}
